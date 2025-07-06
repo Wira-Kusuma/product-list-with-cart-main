e8791f7ec0a6f5ef929c91220e46d94cfea850f7
Wira Kusuma Phandawa <wirakusuma5566@gmail.com>
2025-07-06 19:29:57 +0700

    your cart container

--- script.js
+++ script.js
@@ -8,19 +8,24 @@ fetch('data.json')
         let category = item.category;
         let price = item.price;
         let img = item.image.desktop;
-        // console.log(name);
-        // console.log(category);
-        // console.log(price);
-        // console.log(img);
 
         const card = document.createElement("div");
         card.classList.add("card")
         card.innerHTML = `
         <img src="${img}" alt=$"name">
+        <button>
+          <img src="assets/images/icon-add-to-cart.svg" alt="add to cart">
+          Add to Cart
+        </button>
+        
         <p>${category}</p>
         <h2>${name}</h2>
         <p>$ ${price}</p>
         `;
+
+        
         dessertList.appendChild(card);
     });
   });
+
+
